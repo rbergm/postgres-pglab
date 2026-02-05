f357e2b042ff06813aee4dc65093047259df43b4
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2026-02-05 12:21:00 +0100

    [pg_lab] Expose internal vfd layout in public API

--- src/include/storage/fd.h
+++ src/include/storage/fd.h
@@ -219,4 +219,24 @@ FileWrite(File file, const void *buffer, size_t amount, off_t offset,
 	return FileWriteV(file, &iov, 1, offset, wait_event_info);
 }
 
+/* pg_lab modification */
+
+typedef struct vfd
+{
+	int			fd;				/* current FD, or VFD_CLOSED if none */
+	unsigned short fdstate;		/* bitflags for VFD's state */
+	ResourceOwner resowner;		/* owner, for automatic cleanup */
+	File		nextFree;		/* link to next free VFD, if in freelist */
+	File		lruMoreRecently;	/* doubly linked recency-of-use list */
+	File		lruLessRecently;
+	off_t		fileSize;		/* current size of file (0 if not temporary) */
+	char	   *fileName;		/* name of file, or NULL for unused VFD */
+	/* NB: fileName is malloc'd, and must be free'd when closing the VFD */
+	int			fileFlags;		/* open(2) flags for (re)opening the file */
+	mode_t		fileMode;		/* mode to pass to open(2) */
+} Vfd;
+
+/* pg_lab additions */
+extern Vfd *GetVfdByFile(File file);
+
 #endif							/* FD_H */
